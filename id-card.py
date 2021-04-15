515f136cd4b14c4ea5e8beb3526b6bf3d8038d47
Cem koyluoglu <61415601+CemRoot@users.noreply.github.com>
2021-04-15 14:33:22 +0300

    id-card.py

---
+++ id-card.py
@@ -0,0 +1,13 @@
+# ID CARD 
+'''
+Name = "Cem"
+Surname = "Koyluoglu"
+Born = "Antalya"
+Birthyear = 1999
+
+print("===============================")
+print("İsim             :",Name)
+print("Surname          :",Surname)
+print("Born-Place       :",Born)
+print("Birthyear        :",Birthyear)
+print("===============================")'''
